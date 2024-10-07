e4814522210f67e65e974f5dc5f96351dd583ee3
377Nika2004 <borcovaveronika337@gmail.com>
2024-10-07 22:50:45 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-
+        // Объявите переменные типа int, byte, short, long, float, double.
+        // Название переменных может быть любым, но если состоит из двух слов и более, должно соответствовать правилу camelCase.
+        // Выведите в консоль значение каждой переменной в формате «Значение переменной … с типом … равно …».
+        int a = 1;
+        byte b = 0;
+        short c = 11;
+        long d = 1000;
+        float e = 1.0f;
+        double f = 1.0;
+        System.out.println("Задача 1");
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
